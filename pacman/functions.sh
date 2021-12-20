9ad35ecc7711b585cf06e522aebe3967231ef8d2
S.M. Ali Fateminia <mfateminia@gmail.com>
2021-12-20 22:15:22 +0100

    Update functions.sh

--- pacman/functions.sh
+++ pacman/functions.sh
@@ -27,3 +27,15 @@ pacman-install () {
     sudo pacman -Syy
     sudo pacman -S $package_name --noconfirm
 }
+
+zsh-install () {
+    #install and configure zsh
+    curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+    #add auto suggestion to zsh
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+}
+
+shell-resources-add () {
+    curl https://raw.githubusercontent.com/mfateminia/linux-scripts/master/pacman/shell-resources >> ~/.zshrc
+    curl https://raw.githubusercontent.com/mfateminia/linux-scripts/master/pacman/functions.sh >> ~/.zshrc
+}
