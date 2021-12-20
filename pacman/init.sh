1cc117d04b9c2d603e64b376d8a1ee8ac07a02d3
S.M. Ali Fateminia <mfateminia@gmail.com>
2021-12-20 20:21:26 +0100

    added aliases and renamed funcs

--- pacman/init.sh
+++ pacman/init.sh
@@ -4,23 +4,24 @@ source ./functions.sh
 faillock --reset
 
 #add new packages from pacman below:
-pacman 'code'
-pacman 'powerline'
-pacman 'powerline-fonts'
-pacman 'dotnet-sdk'
-pacman 'dotnet-sdk-3.1'
-pacman 'aspnet-runtime'
-pacman 'aspnet-runtime-3.1'
-pacman 'gnome-keyring'
+pacman-install 'code'
+pacman-install 'powerline'
+pacman-install 'powerline-fonts'
+pacman-install 'dotnet-sdk'
+pacman-install 'dotnet-sdk-3.1'
+pacman-install 'aspnet-runtime'
+pacman-install 'aspnet-runtime-3.1'
+pacman-install 'gnome-keyring'
 
 #add new packages from aur below:
-aur 'nvm'
-aur 'google-chrome'
-aur 'storageexplorer'
+aur-install 'nvm'
+aur-install 'google-chrome'
+aur-install 'storageexplorer'
 
 #content to be appended to .bashrc
 #any new content that needs to be appended to .bashrc should be added to .bashrc in this folder
 curl https://raw.githubusercontent.com/mfateminia/linux-scripts/master/pacman/.bashrc >> ~/.bashrc
+curl https://raw.githubusercontent.com/mfateminia/linux-scripts/master/pacman/functions.sh >> ~/.bashrc
 
 #NuGet authorization package
 wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
