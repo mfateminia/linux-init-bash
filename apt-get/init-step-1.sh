ab8d9baf8d3a9e5df8dd546f39f5fc6cf4d0af3b
S.M. Ali Fateminia <mfateminia@gmail.com>
2022-03-26 23:19:49 +0100

    Update init-step-1.sh

--- apt-get/init-step-1.sh
+++ apt-get/init-step-1.sh
@@ -53,6 +53,11 @@ read user_answer_to_confirm
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 	sudo apt update
 	sudo apt install gh
+	
+#python
+	sudo apt update
+	sudo apt install python3-pip
+	pip install pipenv
 
 #zsh
 	sudo apt-get update
