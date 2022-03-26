36f168111f1b8dabd4a333327b4825686fedd134
S.M. Ali Fateminia <mfateminia@gmail.com>
2022-03-26 15:22:38 +0100

    Update init-step-2.sh

--- apt-get/init-step-2.sh
+++ apt-get/init-step-2.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 # zsh font and theme
-  sudo apt-get install powerline fonts-powerline -y
-  sudo apt-get install zsh-theme-powerlevel9k -y
-  echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
-  sudo apt-get install zsh-syntax-highlighting -y
-  echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  cd ~
+  
 # modifications
 cat >> ~/.zshrc << EOL
-  #remove excess stuff from zshbash
-  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
-  POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
-  POWERLEVEL9K_DISABLE_RPROMPT=true
-  
   # NVM 
   export NVM_DIR="$HOME/.nvm"
   [ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh" # This loads nvm
